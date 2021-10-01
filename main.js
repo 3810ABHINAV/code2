04a975f1c6f515b666811fafa0b4e6568efd24bb
3810ABHINAV <80142530+3810ABHINAV@users.noreply.github.com>
2021-10-01 13:04:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+ing="";
+function preload (){
+img=loadImage("dog_cat.jpg")
+  
+}
+function setup (){
+  canvas=createCanvas(640,420);
+  canvas.center ();
+}
+
+function draw (){
+  Image (img,0,0,640,420)
+  fill("#0000FF")
+  
+}
